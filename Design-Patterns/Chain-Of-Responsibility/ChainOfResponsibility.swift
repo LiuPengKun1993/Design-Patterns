839ab3cd7d1aef727175a25477ccc0afa2094e27
刘朋坤 <pengkun@7net.cc>
2020-05-08 18:31:18 +0800

    责任链模式

---
+++ Design-Patterns/Chain-Of-Responsibility/ChainOfResponsibility.swift
@@ -0,0 +1,104 @@
+//
+//  ChainOfResponsibility.swift
+//  Design-Patterns
+//
+//  Created by 彭倩倩 on 2020/5/8.
+//  Copyright © 2020 刘朋坤. All rights reserved.
+//
+
+import UIKit
+
+class ChainOfResponsibility: NSObject {
+
+    var position: String = ""
+    var bobArray: [String] = ["发现了启动速度慢的问题"]
+    var samArray: [String] = ["发现了启动速度慢的问题", "发现了闪退问题"]
+    var lindaArray: [String] = ["发现了启动速度慢的问题", "发现了卡顿问题"]
+
+    func fixResult(str: String) {
+        print(self.position, str)
+    }
+    
+}
+
+class UserKaren: ChainOfResponsibility {
+    override var position: String {
+        set {
+            let tempPosition = "产品 Karen:"
+            super.position = tempPosition
+        }
+        get {
+            return "产品 Karen:"
+        }
+    }
+    func fixBug(str: String) -> Bool {
+        fixResult(str: str)
+        return DeveloperBob().fixBug(str: str)
+    }
+}
+
+class DeveloperBob: ChainOfResponsibility {
+    override var position: String {
+        set {
+            let tempPosition = "工程师 Bob:"
+            super.position = tempPosition
+        }
+        get {
+            return "工程师 Bob:"
+        }
+    }
+    
+    func fixBug(str: String) -> Bool {
+        if (bobArray.contains(str)) {
+            fixResult(str: "我来解决这个 bug -> \(str)")
+            return true
+        } else {
+            fixResult(str: "不太清楚，可能工程师 Sam 比较熟悉...")
+            return DeveloperSam().fixBug(str: str)
+        }
+    }
+}
+
+class DeveloperSam: ChainOfResponsibility {
+
+    override var position: String {
+        set {
+            let tempPosition = "工程师 Sam:"
+            super.position = tempPosition
+        }
+        get {
+            return "工程师 Sam:"
+        }
+    }
+    func fixBug(str: String) -> Bool {
+        if (samArray.contains(str)) {
+            fixResult(str: "我来解决这个 bug -> \(str)")
+            return true
+        } else {
+            fixResult(str: "不太清楚，可能工程师 Linda 比较熟悉...")
+            return DeveloperLinda().fixBug(str: str)
+        }
+    }
+}
+
+class DeveloperLinda: ChainOfResponsibility {
+
+    override var position: String {
+        set {
+            let tempPosition = "工程师 Linda:"
+            super.position = tempPosition
+        }
+        get {
+            return "工程师 Linda:"
+        }
+    }
+    func fixBug(str: String) -> Bool {
+        if (lindaArray.contains(str)) {
+            fixResult(str: "我来解决这个 bug -> \(str)")
+            return true
+        } else {
+            fixResult(str: "不太清楚，建议先保留此问题")
+            return false
+        }
+    }
+}
--- Design-Patterns/ViewController.swift
+++ Design-Patterns/ViewController.swift
@@ -12,7 +12,42 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        chainOfResponsibility()
+    }
+    
+    // MARK: 责任链模式
+    func chainOfResponsibility() {
+        let userKaren = UserKaren.init()
+        
+        print("------------问题1------------")
+
+        if (userKaren.fixBug(str: "发现了启动速度慢的问题")) {
+            userKaren.fixResult(str: "启动速度慢的问题已解决")
+        } else {
+            userKaren.fixResult(str: "启动速度慢的问题暂时保留")
+        }
+        
+        print("------------问题2------------")
+        if (userKaren.fixBug(str: "发现了闪退问题")) {
+            userKaren.fixResult(str: "闪退问题已解决")
+        } else {
+            userKaren.fixResult(str: "闪退问题暂时保留")
+        }
+        
+        print("------------问题3------------")
+        if (userKaren.fixBug(str: "发现了卡顿问题")) {
+            userKaren.fixResult(str: "卡顿问题已解决")
+        } else {
+            userKaren.fixResult(str: "卡顿问题暂时保留")
+        }
+        
+        print("------------问题4------------")
+        if (userKaren.fixBug(str: "发现了适配问题")) {
+            userKaren.fixResult(str: "适配问题已解决")
+        } else {
+            userKaren.fixResult(str: "适配问题暂时保留")
+        }
     }
 
 
